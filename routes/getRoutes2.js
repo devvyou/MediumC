050c4aa3f16b3ca300e83f7f9b9baf52f1536780
devvyou <devvyou@gmail.com>
2021-09-13 23:13:37 +0200

    Update getRoutes2.js

--- routes/getRoutes2.js
+++ routes/getRoutes2.js
@@ -2,8 +2,19 @@ const csurf = require('csurf'),
     { Router } = require('express'),
     router = Router();
 
+const Admin = require('../database/models/Admin');
+
 router.get('/', (req, res) => {
-    res.render('home')
+    res.send(Admin)
+})
+
+router.get('/b', async (req, res) => {
+    const admin = await Admin.findOne({ email: process.env.ADMIN_EMAIL });
+    return res.render('home', {
+        layout: 'layouts/footerAos',
+        msg: admin[0].broadcast,
+        title: 'MediumC | Landing Page'
+    })
 })
 
 module.exports = router;
